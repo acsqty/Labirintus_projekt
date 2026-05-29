c97a4590996692624e3044d4daa6879923ea6188
Ács Attila <attilaacs4@gmail.com>
2026-05-29 20:08:27 +0200

    Második próba map

--- Palyatervezo/obj/Release/net8.0-windows/Palyatervezo.AssemblyInfo.cs
+++ Palyatervezo/obj/Release/net8.0-windows/Palyatervezo.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Palyatervezo")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f041520b2ff121df36a5e00e7497235e84a5d1b4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Palyatervezo")]
